21470baf1c38276342aaeb09ccc61be580465649
ross <ross@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-01-13 00:11:08 +0000

    Added Spring support module

---
+++ integration/spring/src/main/java/org/mule/galaxy/spring/config/GalaxyApplicationContext.java
@@ -0,0 +1,78 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.galaxy.spring.config;
+
+import org.mule.galaxy.config.ConfigurationSupport;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * A Mule Configuration Builder used for configuring a Mule instance from a Galaxy Registry URL. The can include Login
+ * credentials, the location of the server and a query string used to locate artifacts in the registry. For example -
+ * <code>
+ * http://admin:admin@localhost:9002/api/registry?q=select artifact where mule.service = 'GreeterUMO'"
+ * </code>
+ *
+ * Note that if the query returns more than one artifact they all get loaded, so its important that the query only returns
+ * 'mule' artifacts and only artifacts targeted for this Mule instance.
+ */
+public class GalaxyApplicationContext extends AbstractXmlApplicationContext
+{
+    /**
+     * logger used by this class
+     */
+    protected transient final Log logger = LogFactory.getLog(GalaxyApplicationContext.class);
+
+    private URL configURL;
+
+    public GalaxyApplicationContext(URL configURL)
+    {
+        super();
+        this.configURL = configURL;
+        refresh();
+    }
+
+    public GalaxyApplicationContext(URL configURL, ApplicationContext applicationContext)
+    {
+        super(applicationContext);
+        this.configURL = configURL;
+        refresh();
+    }
+
+    @Override
+    protected Resource[] getConfigResources()
+    {
+        try
+        {
+            ConfigurationSupport configSupport = new ConfigurationSupport();
+            org.mule.galaxy.config.Resource[] resources = configSupport.getArtifacts(configURL.toString(), null);
+
+            InputStreamResource[] springResources = new InputStreamResource[resources.length];
+            for (int i = 0; i < resources.length; i++)
+            {
+                springResources[i] = new InputStreamResource(resources[i].getInputStream(), resources[i].getName());
+            }
+            return springResources;
+        }
+        catch (IOException e)
+        {
+            throw new BeanCreationException("Failed to get Spring beans from The registry: " + e.getMessage(), e);
+        }
+    }
+}
---
+++ integration/spring/src/test/java/org/mule/galaxy/spring/config/GalaxyApplicationContextTest.java
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.galaxy.spring.config;
+
+import org.mule.galaxy.test.AbstractAtomTest;
+
+import java.net.URL;
+
+/**
+ * TODO
+ */
+public class GalaxyApplicationContextTest extends AbstractAtomTest
+{
+    private GalaxyApplicationContext context;
+
+    public void testMuleConfig() throws Exception
+    {
+
+        String configURL = "http://admin:admin@localhost:9002/api/registry?q=select artifact where spring.bean.id = 'TestObject1'";
+
+        context = new GalaxyApplicationContext(new URL(configURL));
+
+        //Assert beans
+        assertNotNull(context.getBean("TestBean1"));
+        assertNotNull(context.getBean("TestBean2"));
+
+    }
+
+
+    @Override
+    protected void tearDown() throws Exception
+    {
+        try
+        {
+            if(context!=null)
+            {
+                context.destroy();
+            }
+        }
+        finally
+        {
+            super.tearDown();
+        }
+    }
+
+    protected String getWebappDirectory()
+    {
+        return "./web/src/main/webapp";
+    }
+}
