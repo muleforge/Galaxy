9dfcc04cafaf23d6120bedeb438aed7325f962a4
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-02-27 18:14:50 +0000

    Merged revisions 494-495,498-509,511-514 via svnmerge from https://svn.muleforge.org/galaxy/branches/galaxy-netboot-multiworkspaces
    ........
      r498 | andrew | 2008-02-26 11:20:43 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Parse netboot and application workspaces config params correctly. Will need to refactor the galaxy communication code next. galaxy.workspace config option is gone.
    ........
      r499 | andrew | 2008-02-26 11:52:11 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Some cleanup of cacheDir vars
    ........
      r500 | andrew | 2008-02-26 12:52:43 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Current refactoring progress: encapsulated workspace operations into a generic Workspace class
    ........
      r501 | andrew | 2008-02-26 13:31:13 -0500 (Tue, 26 Feb 2008) | 1 line
      
      A more specialized netboot workspace class, less ugly...
    ........
      r502 | andrew | 2008-02-26 13:53:40 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Process multiple workspaces in parallel. Nice :)
    ........
      r503 | andrew | 2008-02-26 16:43:19 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Added some classpath debug output. Need to use a barrier and job weighs after all - workspaces can get mixed up on the classpath at the moment.
    ........
      r504 | andrew | 2008-02-26 16:55:44 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Make each workspace processing task use its own executor and completion services. This way they are guaranteed to never get mixed up (workspaces could pick up other workspaces' results sometimes).
    ........
      r505 | andrew | 2008-02-26 17:19:32 -0500 (Tue, 26 Feb 2008) | 1 line
      
      Default to a query selecting jars
    ........
      r507 | andrew | 2008-02-27 11:22:48 -0500 (Wed, 27 Feb 2008) | 1 line
      
      A working support for multiple workspaces. Added *after* netboot workspace to the classpath in the order specified by the user. Nested workspace paths work (e.g. MyWorkspace/Dev/Poc). Local caching needs some minor tweaks to properly reflect the nested workspaces structure. Offline support for multiple workspaces is a TODO for now
    ........
      r508 | andrew | 2008-02-27 11:39:10 -0500 (Wed, 27 Feb 2008) | 1 line
      
      Local cache hierarchy now matches that of boot workspaces
    ........
      r509 | andrew | 2008-02-27 11:39:50 -0500 (Wed, 27 Feb 2008) | 1 line
      
      Cleanup
    ........
      r511 | andrew | 2008-02-27 12:16:02 -0500 (Wed, 27 Feb 2008) | 1 line
      
      Yay, local caching for multiple workspaces now works. That was a smooth sail in groovy :)
    ........
      r512 | andrew | 2008-02-27 12:18:28 -0500 (Wed, 27 Feb 2008) | 1 line
      
      Rethrow a root cause in the launcher
    ........
      r513 | andrew | 2008-02-27 12:54:58 -0500 (Wed, 27 Feb 2008) | 1 line
      
      Properly shutdown executor services on error, ensures a process is terminated and doesn't hang
    ........
      r514 | andrew | 2008-02-27 13:05:41 -0500 (Wed, 27 Feb 2008) | 1 line
      
      Implicit last-statement-is-the-return-value doesn't apply to try/finally blocks.
    ........

--- netboot/mule1/support/src/main/java/org/mule/galaxy/netboot/GalaxyBootstrap.groovy
+++ netboot/mule1/support/src/main/java/org/mule/galaxy/netboot/GalaxyBootstrap.groovy
@@ -1,35 +1,37 @@
 /*
- * $Id$
- * --------------------------------------------------------------------------------------
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+* $Id$
+* --------------------------------------------------------------------------------------
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
-import org.apache.commons.httpclient.methods.GetMethod
-import org.mule.galaxy.client.Galaxy
-import java.util.concurrent.Executors
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Callable
+package org.mule.galaxy.netboot
+
 import java.util.concurrent.ExecutorCompletionService
-import java.text.SimpleDateFormat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import org.mule.galaxy.client.Galaxy
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutionException
 
 class GalaxyBootstrap {
 
     def static g
-    def static workspace
+    def static List workspaces
+    def static netBootWorkspace
     def static boolean debug
-    def static File cacheDir
+    def static File netBootCacheDir
 
     def static numUnits = Runtime.runtime.availableProcessors() * 4
 
@@ -51,15 +53,18 @@ class GalaxyBootstrap {
         def apiUrl = p.'galaxy.apiUrl' ?: '/api/registry'
         def username = p.'galaxy.username' ?: 'admin'
         def password = p.'galaxy.password' ?: 'admin'
-        workspace = p.'galaxy.workspace' ?: 'Mule'
+        // split by comma, prune duplicates, all in a null-safe manner
+        workspaces = p.'galaxy.app.workspaces'?.split(',')?.toList()?.unique() ?: []
+        netBootWorkspace = p.'galaxy.netboot.workspace' ?: 'Mule'
         debug = 'true'.equalsIgnoreCase(p.'galaxy.debug')
 
         // Passed in as -Dmule.home
         def muleHome = p.'mule.home'
         // create a local cache dir if needed
-        cacheDir = new File(muleHome, "lib/cache/$workspace")
-        cacheDir.mkdirs()
-        assert cacheDir.exists()
+        File cacheDir = new File(muleHome, 'lib/cache')
+        netBootCacheDir = new File(cacheDir, netBootWorkspace)
+        netBootCacheDir.mkdirs()
+        assert netBootCacheDir.exists()
 
         println """\n
 ${'=' * 78}
@@ -67,7 +72,8 @@ MULE_HOME: $muleHome
 Local Jar Cache: $cacheDir.canonicalPath
 Galaxy URL: $httpScheme://$host:$port$apiUrl
 Username: $username
-Workspace: $workspace
+NetBoot Workspace: $netBootWorkspace
+Application Workspaces: $workspaces
 Proc Units: $numUnits
 Debug: $debug
 ${'=' * 78}
@@ -94,102 +100,96 @@ Fetching artifacts from Galaxy...
                 clean classpath from the cache.
              */
             def cachedUrls = []
-            cachedUrls += processWorkspace('user')
-            cachedUrls += processWorkspace('mule')
-            cachedUrls += processWorkspace('opt')
 
-            urls += cachedUrls
-        } catch (ConnectException cex) {
+            /*
+                Process workspaces in parallel below
+            */
+
+            // lib/user
+            def libUser = exec.submit({
+                new NetBootWorkspace(galaxy: g,
+                                  name: 'user',
+                                  netBootWorkspace: netBootWorkspace,
+                                  netBootCacheDir: netBootCacheDir.canonicalPath).init().process()
+            } as Callable)
+
+            // lib/mule
+            def libMule = exec.submit({
+                    new NetBootWorkspace(galaxy: g,
+                                  name: 'mule',
+                                  netBootWorkspace: netBootWorkspace,
+                                  netBootCacheDir: netBootCacheDir.canonicalPath).init().process()
+            } as Callable)
+
+            // lib/opt
+            def libOpt = exec.submit({
+                    new NetBootWorkspace(galaxy: g,
+                                  name: 'opt',
+                                  netBootWorkspace: netBootWorkspace,
+                                  netBootCacheDir: netBootCacheDir.canonicalPath).init().process()
+            } as Callable)
+
+
+            /*
+                Now process application workspaces
+            */
+
+            def appWsFutures = []
+            workspaces.each { String ws ->
+                def name = ws.contains('/') ? ws.substring(ws.lastIndexOf('/') + 1) : ws
+                def parentWorkspace = ws.contains('/') ? ws.substring(0, ws.lastIndexOf('/')) : ''
+
+
+                appWsFutures << exec.submit({
+                    File file = new File(cacheDir, parentWorkspace)
+                    new Workspace(galaxy: g,
+                                  name: name,
+                                  parentWorkspace: parentWorkspace,
+                                  cacheDir: file.canonicalPath).process()
+                } as Callable)
+            }
+
+            urls += libUser.get() + libMule.get() + libOpt.get()
+            // add application workspaces' urls
+            appWsFutures.each { urls += it.get() }
+
+        } catch (ExecutionException eex) { // wraps any execution job
+
+            def rootCause = eex.cause?.cause // 1st cause is groovy invoker exception, next is the real one
+
+            if (!(rootCause instanceof ConnectException)) {
+                // fail-fast and rethrow
+                throw rootCause
+            }
+
             println "Galaxy server is not available, will try a local cache..."
-            new File(cacheDir, 'lib/user').listFiles().each { file ->
-                urls << file.toURI().toURL()
+
+            def fromCache = { subDir ->
+                new File(cacheDir, subDir).listFiles().each { file ->
+                    urls << file.toURI().toURL()
+                }
             }
-            new File(cacheDir, 'lib/mule').listFiles().each { file ->
-                urls << file.toURI().toURL()
-            }
-            new File(cacheDir, 'lib/opt').listFiles().each { file ->
-                urls << file.toURI().toURL()
+
+            // netboot cache
+            fromCache("$netBootWorkspace/lib/user")
+            fromCache("$netBootWorkspace/lib/mule")
+            fromCache("$netBootWorkspace/lib/opt")
+
+            // app workspaces cache
+            workspaces.each { name ->
+                fromCache(name)
             }
         }
+        finally {
+            exec.shutdown()
+        }
 
-        exec.shutdown()
+        if (debug) { println urls.join('\n') }
 
         println '\nLaunching Mule, get set for a hyper-jump...\n'
 
         urls.toArray(new URL[urls.size()])
     }
 
-    private static processWorkspace(name) {
-        GetMethod response = g.get("$workspace/lib/$name")
-
-        // local cache dir
-        def dir = new File(cacheDir, "lib/$name")
-        dir.mkdirs()
-        assert dir.exists()
-
-        def feed = new XmlSlurper().parse(response.responseBodyAsStream)
-
-        int totalCount = 0
-        feed.entry.each {
-            // close on a local var for it to be accessible in Callable, fixes NPE
-            def node = it
-            def task = {
-                URL url = new URL("http://${g.host}:$g.port${node.content.@src}")
-
-                def jarName = node.title.text()
-
-                // TODO plug voters here to decide if it needs to be downloaded really
-                // cache jars locally
-                File localJar = new File(dir, jarName)
-
-                if (lastUpdatedVote (localJar, node)) {
-                    println "Updating a local copy of $jarName"
-                    GetMethod content = g.get("$workspace/lib/$name/$jarName")
-                    localJar.newOutputStream() << content.responseBodyAsStream
-                    content.releaseConnection()
-                }
-
-                // fix broken URLs for File class before Java 6
-                return localJar.toURI().toURL()
-            } as Callable
-
-            compService.submit task
-
-            totalCount++
-        } 
-
-        // locally cached jar urls
-        def urls = []
-        totalCount.times {
-            urls << compService.take().get()
-        }
-
-        response.releaseConnection()
-
-        urls
-    }
-
-    // return true if update from Galaxy is required
-    def static boolean lastUpdatedVote (File localFile, atomEntryNode) {
-        boolean vote = false
-        if (!localFile || !localFile.exists()) {
-            vote = true
-        }
-
-        // not thread-safe, create a local instance
-        // Galaxy returns update timestamps in GMT
-        def iso8601Date = new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:sss")
-        iso8601Date.setTimeZone(TimeZone.getTimeZone('GMT'))
-
-        Date galaxyUpdate = iso8601Date.parse (atomEntryNode.updated.text())
-
-        final Date localFileUpdate = new Date(localFile.lastModified())
-        vote = galaxyUpdate.after(localFileUpdate)
-        if (debug) {
-            println "$localFile.name >>$vote<<  galaxyUpdate: $galaxyUpdate || localFile: ${localFileUpdate}"
-        }
-
-        return vote
-    }
 }
     
