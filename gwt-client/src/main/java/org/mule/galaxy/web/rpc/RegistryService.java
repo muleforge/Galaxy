24dea3bef9b0376836c1b16275701ca6866099ee
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-04-18 02:03:29 +0000

    Revert r760, will need to be revisited (breaks with serialization error after login ?)

--- gwt-client/src/main/java/org/mule/galaxy/web/rpc/RegistryService.java
+++ gwt-client/src/main/java/org/mule/galaxy/web/rpc/RegistryService.java
@@ -39,7 +39,6 @@ public interface RegistryService extends RemoteService {
     
     /**
      * @gwt.typeArgs searchPredicates <org.mule.galaxy.web.rpc.SearchPredicate>
-     * @gwt.typeArgs artifactTypes <org.mule.galaxy.web.rpc.WArtifactType>
      * @param start TODO
      * @param maxResults TODO
      * @return 
@@ -81,7 +80,10 @@ public interface RegistryService extends RemoteService {
                         String propertyName) throws RPCException, ItemNotFoundException;
     
     /**
-     * @throws ItemNotFoundException
+     * @gwt.typeArgs <org.mule.galaxy.web.rpc.WPropertyDescriptor>
+     * @return
+     * @throws ItemNotFoundException 
+     * @throws Exception 
      */
     void savePropertyDescriptor(WPropertyDescriptor property) throws RPCException, ItemNotFoundException;
     
