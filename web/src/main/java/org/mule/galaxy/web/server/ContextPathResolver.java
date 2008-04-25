53a1dffcb08e99c31db403fef262e318f5b55e70
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-04-25 13:34:17 +0000

    GALAXY-194 Galaxy presumes war is deployed in the root context. A more robust solution working in more scenarios. This got even worse than trying to make CSS look consistent across browsers, the difference being it's for servers now.

--- web/src/main/java/org/mule/galaxy/web/server/ContextPathResolver.java
+++ web/src/main/java/org/mule/galaxy/web/server/ContextPathResolver.java
@@ -18,15 +18,27 @@
 
 package org.mule.galaxy.web.server;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Correctly resolves root context path both for WAR deployments in root/non-root contexts,
- * as well as for test runs.
+ * as well as for test runs. There's some inconsistent behavior across servlet containers
+ * in this regard, and then the case is complicated by embedded and test runs of Galaxy.
+ * You may need to deploy a related filter for things to work in some scenarios (already
+ * done in Galaxy out-of-the-box).
+ *
+ * @see org.mule.galaxy.web.ContextPathSaverFilter
  */
 public class ContextPathResolver
 {
+    /**
+     * A resolved context path. Optional, may never get set in some embedded scenarios.
+     */
+    private final static AtomicReference<String> savedContextPath = new AtomicReference<String>(null);
+
     /**
      * Returns:
      * <ul>
@@ -38,7 +50,17 @@ public class ContextPathResolver
      */
     public String getContextPath()
     {
-        String path = "";
+        String path = savedContextPath.get();
+        if (path != null)
+        {
+            // we're running in a container, filter intercepted the context path
+            return path;
+        } else
+        {
+            path = "";
+        }
+
+        // otherwise, let's try to guess, as we're running either embedded or in a test
         WebApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
         if (context != null)
         {
@@ -50,5 +72,14 @@ public class ContextPathResolver
             }
         }
         return path;
+    }
+
+    /**
+     * Normally called by a {@link org.mule.galaxy.web.ContextPathSaverFilter}. 
+     * @param path resolved context path
+     */
+    public static void saveContextPath(String path)
+    {
+        savedContextPath.compareAndSet(null, path);
     }
 }
