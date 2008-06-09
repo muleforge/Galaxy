db136308476218e7af3b10f4fe682e8a13c96aef
merv <merv@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-06-09 13:31:27 +0000

    Remove unused imports.

--- web/src/main/java/org/mule/galaxy/web/ArtifactUploadServlet.java
+++ web/src/main/java/org/mule/galaxy/web/ArtifactUploadServlet.java
@@ -21,8 +21,6 @@ package org.mule.galaxy.web;
 import org.mule.galaxy.Artifact;
 import org.mule.galaxy.ArtifactPolicyException;
 import org.mule.galaxy.ArtifactResult;
-import org.mule.galaxy.ContentHandler;
-import org.mule.galaxy.ContentService;
 import org.mule.galaxy.DuplicateItemException;
 import org.mule.galaxy.NotFoundException;
 import org.mule.galaxy.Registry;
@@ -33,19 +31,14 @@ import org.mule.galaxy.policy.ApprovalMessage;
 import org.mule.galaxy.security.AccessException;
 import org.mule.galaxy.security.User;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
-import javax.activation.MimeType;
 import javax.activation.MimeTypeParseException;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -56,17 +49,15 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.lang.BooleanUtils;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 public class ArtifactUploadServlet implements Controller {
-    
+
     private Registry registry;
 
     public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp)
-        throws Exception {
+            throws Exception {
         String artifactId = null;
         String wkspcId = null;
         String name = null;
@@ -74,21 +65,21 @@ public class ArtifactUploadServlet implements Controller {
         String contentType = null;
         FileItem uploadItem = null;
         boolean disablePrevious = false;
-        
+
         resp.setContentType("text/plain");
 
         PrintWriter writer = resp.getWriter();
-        
+
         try {
             FileItemFactory factory = new DiskFileItemFactory();
             ServletFileUpload upload = new ServletFileUpload(factory);
-            
+
             try {
                 List items = upload.parseRequest(req);
                 Iterator it = items.iterator();
                 while (it.hasNext()) {
-                    FileItem item = (FileItem)it.next();
-                    
+                    FileItem item = (FileItem) it.next();
+
                     String f = item.getFieldName();
 
                     if ("artifactFile".equals(f)) {
@@ -102,14 +93,14 @@ public class ArtifactUploadServlet implements Controller {
                         versionLabel = item.getString();
                     } else if ("artifactId".equals(f)) {
                         artifactId = item.getString();
-                    }  else if ("disablePrevious".equals(f)) {
+                    } else if ("disablePrevious".equals(f)) {
                         disablePrevious = BooleanUtils.toBoolean(item.getString());
                     }
                 }
             } catch (FileUploadException e) {
                 throw new ServletException(e);
             }
-            
+
             if (uploadItem == null) {
                 writer.write("No file was specified.");
                 return null;
@@ -120,45 +111,45 @@ public class ArtifactUploadServlet implements Controller {
                 return null;
             }
 
-            UserDetailsWrapper wrapper = (UserDetailsWrapper)SecurityContextHolder.getContext()
-                .getAuthentication().getPrincipal();
+            UserDetailsWrapper wrapper = (UserDetailsWrapper) SecurityContextHolder.getContext()
+                    .getAuthentication().getPrincipal();
             User user = wrapper.getUser();
-            
+
             ArtifactResult result = null;
-            if (artifactId == null) {            
+            if (artifactId == null) {
                 if (wkspcId == null) {
                     writer.write("No workspace was specified.");
                     return null;
                 }
-                
+
                 Workspace wkspc = registry.getWorkspace(wkspcId);
-                
+
                 // pull out the original file name
                 if (name == null || "".equals(name)) {
                     name = uploadItem.getName();
-                    
+
                     int idx = name.lastIndexOf('/');
                     if (idx == -1) {
                         idx = name.lastIndexOf('\\');
                     }
-                    name = name.substring(idx+1);
+                    name = name.substring(idx + 1);
                 }
-                
+
                 if (contentType == null) {
                     contentType = "application/octet-stream";
                 }
-                
+
                 result = registry.createArtifact(wkspc, contentType, name, versionLabel, uploadItem.getInputStream(), user);
             } else {
                 Artifact a = registry.getArtifact(artifactId);
-                
+
                 result = registry.newVersion(a, uploadItem.getInputStream(), versionLabel, user);
-                
+
                 if (disablePrevious) {
                     result.getArtifactVersion().getPrevious().setEnabled(false);
                 }
             }
-            
+
             writer.write("OK " + result.getArtifact().getId());
         } catch (NotFoundException e) {
             writer.write("Workspace could not be found.");
@@ -166,7 +157,7 @@ public class ArtifactUploadServlet implements Controller {
             writer.write("No version label was specified.");
         } catch (ArtifactPolicyException e) {
             writer.write("ArtifactPolicyException\n");
-            
+
             List<ApprovalMessage> approvals = e.getApprovals();
             for (ApprovalMessage a : approvals) {
                 if (a.isWarning()) {
@@ -177,13 +168,13 @@ public class ArtifactUploadServlet implements Controller {
                 writer.write(a.getMessage());
                 writer.write("\n");
             }
-            
+
             Collections.sort(approvals, new Comparator<ApprovalMessage>() {
 
                 public int compare(ApprovalMessage o1, ApprovalMessage o2) {
                     return o1.getMessage().compareTo(o2.getMessage());
                 }
-                
+
             });
         } catch (MimeTypeParseException e) {
             writer.write("Invalid mime type.");
