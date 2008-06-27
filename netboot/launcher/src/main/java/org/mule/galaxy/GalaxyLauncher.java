98b4cd399a71bb27654db76c239d7e51e7d84c44
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-06-27 17:06:03 +0000

    Merged revisions 1078-1083,1087,1092,1094,1097,1101,1103,1105-1107,1110 via svnmerge from https://svn.muleforge.org/galaxy/trunk
    ........
      r1078 | ross | 2008-06-19 09:47:47 -0400 (Thu, 19 Jun 2008) | 1 line
      
      start of GALAXTYEE-15. separate the config options into an objec that can be passed around
    ........
      r1079 | andrew | 2008-06-19 16:17:47 -0400 (Thu, 19 Jun 2008) | 1 line
      
      Use system-specific newline chars (Groovy automatically puts the right one when \n is used, but not Java)
    ........
      r1080 | andrew | 2008-06-19 16:19:59 -0400 (Thu, 19 Jun 2008) | 1 line
      
      No duplicate validation call
    ........
      r1081 | andrew | 2008-06-19 16:29:59 -0400 (Thu, 19 Jun 2008) | 1 line
      
      Finish the port of p.'galaxy.app.workspaces'?.split(',')?.toList()?.unique()
    ........
      r1082 | andrew | 2008-06-19 16:32:41 -0400 (Thu, 19 Jun 2008) | 1 line
      
      Use property-style refs, easier on the eye
    ........
      r1083 | andrew | 2008-06-19 16:36:06 -0400 (Thu, 19 Jun 2008) | 1 line
      
      Bootstrap had List type for workspaces, new NetbootConfig has String[]. Match those
    ........
      r1087 | andrew | 2008-06-24 10:51:48 -0400 (Tue, 24 Jun 2008) | 1 line
      
      Database filesystems don't have a path config attribute
    ........
      r1110 | dandiep | 2008-06-27 11:21:49 -0400 (Fri, 27 Jun 2008) | 1 line
      
      svn ignores
    ........

--- netboot/launcher/src/main/java/org/mule/galaxy/GalaxyLauncher.java
+++ netboot/launcher/src/main/java/org/mule/galaxy/GalaxyLauncher.java
@@ -19,24 +19,58 @@
 package org.mule.galaxy;
 
 import org.mule.galaxy.netboot.GalaxyClientClassPathConfig;
+import org.mule.galaxy.netboot.NetbootConfig;
 import org.mule.galaxy.netboot.launch.NamedUrlClassLoader;
 
 import java.io.File;
-import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.List;
 
-import org.tanukisoftware.wrapper.WrapperManager;
 import org.tanukisoftware.wrapper.WrapperListener;
+import org.tanukisoftware.wrapper.WrapperManager;
 
 public class GalaxyLauncher
 {
+    /**
+     * logger used by this class
+     */
     public static final String GALAXY_BOOTSTRAP_CLASS_NAME = "org.mule.galaxy.netboot.GalaxyBootstrap";
 
     public static void main(String[] args) throws Throwable
     {
-        GalaxyClientClassPathConfig classpath = new GalaxyClientClassPathConfig(lookupMuleHome());
+
+
+        NetbootConfig config = new NetbootConfig(args, System.getProperties());
+        final GalaxyLauncher launcher = new GalaxyLauncher();
+        System.out.println("Mule Netboot started");
+
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            //@Override
+            public void run()
+            {
+                launcher.shutdown();
+            }
+
+        });
+
+        launcher.launchWithConfig(config);
+    }
+
+
+
+    public void shutdown()
+    {
+        System.out.println("Mule Netboot Shutting down...");
+    }
+
+
+    public void launchWithConfig(NetbootConfig config) throws Exception
+    {
+        config.validate();
+        GalaxyClientClassPathConfig classpath = new GalaxyClientClassPathConfig(new File(config.getMuleHome()));
 
         final List<URL> urlList = classpath.getURLs();
         URL[] galaxyJars = urlList.toArray(new URL[urlList.size()]);
@@ -47,18 +81,18 @@ public class GalaxyLauncher
         ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(galaxyCl);
 
-        // call out to external class interacting with Galaxy to construct the final set of jar URLs 
+        // call out to external class interacting with Galaxy to construct the final set of jar URLs
         Class galaxyBootstrap = Class.forName(GALAXY_BOOTSTRAP_CLASS_NAME, true, Thread.currentThread().getContextClassLoader());
-        Method method = galaxyBootstrap.getMethod("constructMuleClasspath");
+        Method method = galaxyBootstrap.getMethod("constructMuleClasspath", NetbootConfig.class);
         URL[] muleClasspathUrls = new URL[0]; // it's a static method
         try
         {
-            muleClasspathUrls = (URL[]) method.invoke(null);
+            muleClasspathUrls = (URL[]) method.invoke(null, config);
         }
         catch (InvocationTargetException e)
         {
             // rethrow a nested root cause
-            throw e.getCause();
+            throw (Exception) e.getCause();
         }
 
         // dereference explicitly to help GC
@@ -68,32 +102,16 @@ public class GalaxyLauncher
 
         System.gc();
 
-        // create Mule's classloader as a child of whatever called us
+// create Mule's classloader as a child of whatever called us
         NamedUrlClassLoader muleCl = new NamedUrlClassLoader(muleClasspathUrls, oldCl);
-        muleCl.setTag("mule launcher");
+        muleCl.setTag("netboot launcher");
         Thread.currentThread().setContextClassLoader(muleCl);
 
         // launch Mule
         Object wrapper = Class.forName("org.mule.galaxy.netboot.launch.MuleServerWrapper",
-                                       true, Thread.currentThread().getContextClassLoader()).newInstance();
-        WrapperManager.start((WrapperListener) wrapper, args);
+                true, Thread.currentThread().getContextClassLoader()).newInstance();
+        WrapperManager.start((WrapperListener) wrapper, config.getStartupArgs());
     }
 
-    private static File lookupMuleHome() throws Exception
-    {
-        File muleHome = null;
-        String muleHomeVar = System.getProperty("mule.home");
-
-        if (muleHomeVar != null && !muleHomeVar.trim().equals("") && !muleHomeVar.equals("%MULE_HOME%"))
-        {
-            muleHome = new File(muleHomeVar).getCanonicalFile();
-        }
-
-        if (muleHome == null || !muleHome.exists() || !muleHome.isDirectory())
-        {
-            throw new IllegalArgumentException("Either MULE_HOME is not set or does not contain a valid directory.");
-        }
-        return muleHome;
-    }
 
 }
