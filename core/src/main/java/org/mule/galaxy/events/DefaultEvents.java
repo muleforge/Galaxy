ee79bd3ce417e30139ad18fa2c91183268181570
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-07-03 20:28:04 +0000

    GALAXY-321 Added a convenience constants class where every standard Galaxy event can be listed for type-safety (Enums don't work nicely with annotations config).

---
+++ core/src/main/java/org/mule/galaxy/events/DefaultEvents.java
@@ -0,0 +1,11 @@
+package org.mule.galaxy.events;
+
+/**
+ * A convenience Constants class for every standard event in Galaxy.
+ */
+public class DefaultEvents {
+
+    public static final String WORKSPACE_CREATED = "WorkspaceCreated";
+    public static final String WORKSPACE_DELETED = "WorkspaceDeleted";
+    
+}
--- core/src/main/java/org/mule/galaxy/events/listeners/ActivityLoggerListener.java
+++ core/src/main/java/org/mule/galaxy/events/listeners/ActivityLoggerListener.java
@@ -1,14 +1,16 @@
 package org.mule.galaxy.events.listeners;
 
+import static org.mule.galaxy.events.DefaultEvents.WORKSPACE_CREATED;
+import static org.mule.galaxy.events.DefaultEvents.WORKSPACE_DELETED;
+import org.mule.galaxy.events.GalaxyEvent;
 import org.mule.galaxy.events.GalaxyEventListener;
 import org.mule.galaxy.events.WorkspaceCreatedEvent;
 import org.mule.galaxy.events.WorkspaceDeletedEvent;
-import org.mule.galaxy.events.GalaxyEvent;
 import org.mule.galaxy.events.annotations.BindToEvents;
 
 @BindToEvents({
-        "WorkspaceCreated",
-        "WorkspaceDeleted"})
+        WORKSPACE_CREATED,
+        WORKSPACE_DELETED})
 // TODO refactor this
 public class ActivityLoggerListener implements GalaxyEventListener {
 
