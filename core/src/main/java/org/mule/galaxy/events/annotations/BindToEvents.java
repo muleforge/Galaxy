16806947b2c89873e54b6d2f32e58f67e3d78551
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-07-04 15:32:07 +0000

    GALAXY-321 Set annotation target to a top-level class only.

--- core/src/main/java/org/mule/galaxy/events/annotations/BindToEvents.java
+++ core/src/main/java/org/mule/galaxy/events/annotations/BindToEvents.java
@@ -1,8 +1,12 @@
 package org.mule.galaxy.events.annotations;
 
+import static java.lang.annotation.ElementType.TYPE;
 import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
 
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Retention(RUNTIME)
+@Target(TYPE)
 public @interface BindToEvents {
     String[] value();
 }
