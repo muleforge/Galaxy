663e7517a4aebae0605bd92586cdc206037c3e8f
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-08-19 21:32:27 +0000

    javadoc

--- api/src/main/java/org/mule/galaxy/Links.java
+++ api/src/main/java/org/mule/galaxy/Links.java
@@ -2,6 +2,17 @@ package org.mule.galaxy;
 
 import java.util.Collection;
 
+/**
+ * An interface to query for links. Many different link relationships
+ * can be created. To do this create a new PropertyDescriptor with the LinkExtension.
+ * 
+ * Links for an Item's property can be accessed by doing the following:
+ * <pre>
+ * Item item = ...;
+ * 
+ * Links links = (Links) item.getProperty("depends");
+ * </pre>
+ */
 public interface Links {
 
     Collection<Link> getLinks();
