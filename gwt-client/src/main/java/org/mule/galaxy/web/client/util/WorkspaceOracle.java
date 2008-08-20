18cc5a2547220973330310fed8bc8a32a6a78477
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-08-20 20:45:13 +0000

    Add missing workspace oracle.

---
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/util/WorkspaceOracle.java
@@ -0,0 +1,63 @@
+package org.mule.galaxy.web.client.util;
+
+import com.google.gwt.user.client.ui.SuggestOracle;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.mule.galaxy.web.client.ErrorPanel;
+import org.mule.galaxy.web.client.Galaxy;
+import org.mule.galaxy.web.rpc.AbstractCallback;
+import org.mule.galaxy.web.rpc.EntryInfo;
+
+public class WorkspaceOracle extends SuggestOracle {
+
+    private Galaxy galaxy;
+    private ErrorPanel errorPanel;
+    private final String exclude;
+    
+    public WorkspaceOracle(Galaxy galaxy, ErrorPanel errorPanel, String exclude) {
+        super();
+        this.errorPanel = errorPanel;
+        this.galaxy = galaxy;
+        this.exclude = exclude;
+    }
+
+    @Override
+    public void requestSuggestions(final Request request, final Callback callback) {
+        galaxy.getRegistryService().suggestWorkspaces(request.getQuery(), exclude,
+            new AbstractCallback<Collection<String>>(errorPanel) {
+
+            public void onSuccess(Collection<String> entries) {
+                updateSuggestions(entries, request, callback);
+            }
+            
+        });
+    }
+
+    protected void updateSuggestions(Collection<String> entries, Request request, Callback callback) {
+        Response response = new Response();
+        List<Suggestion> suggestions = new ArrayList<Suggestion>();
+        for (final String e : entries) {
+            suggestions.add(new Suggestion() {
+                public String getDisplayString() {
+                    return e;
+                }
+
+                public String getReplacementString() {
+                    return e;
+                }
+            });
+        }
+        response.setSuggestions(suggestions);
+        callback.onSuggestionsReady(request, response);
+    }
+
+    @Override
+    public boolean isDisplayStringHTML() {
+        return true;
+    }
+
+}
