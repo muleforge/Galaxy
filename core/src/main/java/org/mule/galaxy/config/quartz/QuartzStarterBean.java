40be981dc1909553027f41fa7d679a869bea22c3
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-09-04 21:28:33 +0000

    Fixed a deadlock on galaxy/quartz startup when there was a durable short-interval job (e.g. every 10 secs), which prevented the rest of the system from initializing.

---
+++ core/src/main/java/org/mule/galaxy/config/quartz/QuartzStarterBean.java
@@ -0,0 +1,52 @@
+package org.mule.galaxy.config.quartz;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class QuartzStarterBean implements ApplicationListener {
+
+    private final Log logger = LogFactory.getLog(getClass());
+
+    private Scheduler scheduler;
+
+    public void setScheduler(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    public Scheduler getScheduler() {
+        return scheduler;
+    }
+
+    public void onApplicationEvent(ApplicationEvent event) {
+        try {
+            if (event instanceof ContextStoppedEvent &&
+                    (scheduler.isStarted() || scheduler.isInStandbyMode())) {
+                scheduler.shutdown();
+                if (logger.isInfoEnabled()) {
+                    logger.info("Successful shutdown of the scheduler");
+                }
+            }
+        } catch (SchedulerException ex) {
+            logger.warn("Failed to shutdown scheduler", ex);
+        }
+
+        try {
+            if ((event instanceof ContextStartedEvent || event instanceof ContextRefreshedEvent)
+                    && !scheduler.isStarted()) {
+                scheduler.start();
+                if (logger.isInfoEnabled()) {
+                    logger.info("Scheduler successfully started");
+                }
+            }
+        } catch (SchedulerException ex) {
+            throw new IllegalStateException("Failed to start the scheduler", ex);
+        }
+    }
+}
