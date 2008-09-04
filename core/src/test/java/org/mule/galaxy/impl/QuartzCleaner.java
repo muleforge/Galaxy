f2e805e98fcd40fa3be9eed52f44296cd78f2035
andrew <andrew@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-09-04 18:17:50 +0000

    Merge my and Dan's changes. Man, you're having too short lunches ;)

--- core/src/test/java/org/mule/galaxy/impl/QuartzCleaner.java
+++ core/src/test/java/org/mule/galaxy/impl/QuartzCleaner.java
@@ -1,5 +1,7 @@
 package org.mule.galaxy.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 
@@ -7,11 +9,18 @@ import org.quartz.SchedulerException;
  * Removes an left over jobs from the scheduler so we always start with a clean db after tests run.
  */
 public class QuartzCleaner {
+
+    private final Log logger = LogFactory.getLog(getClass());
+
     private Scheduler scheduler;
     
     public void clean() throws SchedulerException {
         for (String group : scheduler.getJobGroupNames()) {
             for (String name : scheduler.getJobNames(group)) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug(String.format("Deleting job: %s in group: %s", name, group));
+                }
+
                 scheduler.deleteJob(name, group);
             }
         }
