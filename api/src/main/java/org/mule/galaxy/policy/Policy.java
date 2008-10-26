3d41f047b953c14436ff8988f564193034be8a70
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2008-10-26 22:45:26 +0000

    policy api docs.

--- api/src/main/java/org/mule/galaxy/policy/Policy.java
+++ api/src/main/java/org/mule/galaxy/policy/Policy.java
@@ -6,21 +6,40 @@ import org.mule.galaxy.Registry;
 import java.util.Collection;
 
 /**
- * An ArtifactPolicy allows custom criteria to be setting a new Active ArtifactVersion. 
- * For instance, you could implement a WSDL versioning policy which enforced backward 
- * compatability.
+ * A Policy allows you to control if items in the registry should be allowed
+ * to progress to new lifecycle transitions or added to the registry at all.
  */
 public interface Policy {
     
+    /**
+     * A unique ID for the policy.
+     */
     String getId();
     
+    /**
+     * A short descriptive name for the policy.
+     */
     String getName();
-    
+
+    /**
+     * A description of the policy
+     */
     String getDescription();
-    
+
+    /**
+     * Whether or not the policy applies to te specified item.
+     */
     boolean applies(Item item);
     
+    /**
+     * Approves an item according to the policy. If the item is not approved,
+     * a Collection of {@link ApprovalMessage}s will be returned. Otherwise,
+     * null or an empty Collection can be returned.
+     */
     Collection<ApprovalMessage> isApproved(Item item);
     
+    /**
+     * Set the {@link Registry} instance for this policy.
+     */
     void setRegistry(Registry registry);
 }
