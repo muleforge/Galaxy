5bec9b588fd32b4d7cf4a26fbe063691d9876a7e
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2009-06-03 19:25:08 +0000

    Upgrade to gwt-sl 0.1.5b

--- gwt-client/src/main/java/org/mule/galaxy/web/client/SessionKilledDialog.java
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/SessionKilledDialog.java
@@ -78,7 +78,7 @@ public class SessionKilledDialog extends DialogBox {
         final InlineFlowPanel mainMessage = new InlineFlowPanel();
         mainMessage.addStyleName("padding");
         final HTML text = new HTML("This client connection has been terminated by the server. This could happen due to either:" +
-                "<ul><li>Server having crashed<li>Client session forcefully killed on the server</ul>" +
+                "<ul><li>Server having crashed</li><li>Client session forcefully killed on the server</li></ul>" +
                 "This error is <strong>unrecoverable</strong> and you'll need to re-login. Next " +
                 "connection attempt will be made in ");
         timerLabel = new Label("" + heartbeatTimer.getIntervalSeconds());
