518b2772190305d283421ef459ffa7cc317e75ac
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2009-08-19 16:51:16 +0000

    Add ability to ignore properties.

--- core/src/main/java/org/mule/galaxy/event/listener/activity/PropertyChangedEventListener.java
+++ core/src/main/java/org/mule/galaxy/event/listener/activity/PropertyChangedEventListener.java
@@ -1,23 +1,37 @@
 package org.mule.galaxy.event.listener.activity;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mule.galaxy.activity.ActivityManager;
-import static org.mule.galaxy.event.DefaultEvents.PROPERTY_CHANGED;
 import org.mule.galaxy.event.PropertyChangedEvent;
 import org.mule.galaxy.event.annotation.Async;
 import org.mule.galaxy.event.annotation.BindToEvent;
 import org.mule.galaxy.event.annotation.OnEvent;
 
-import java.text.MessageFormat;
+import static org.mule.galaxy.event.DefaultEvents.PROPERTY_CHANGED;
 
 @BindToEvent(PROPERTY_CHANGED)
 public class PropertyChangedEventListener extends AbstractActivityLoggingListener {
 
+    private List<String> ignoreProperties = new ArrayList<String>();
+    
     @OnEvent
     @Async
     public void onEvent(PropertyChangedEvent event) {
+        if (ignoreProperties.contains(event.getPropertyName())) {
+            return;
+        }
+        
         final String message = MessageFormat.format(
                 "* Property {0} of {1} was set to {2}", event.getPropertyName(), event.getItemPath(), event.getNewValue());
 
         getActivityManager().logActivity(message, ActivityManager.EventType.INFO, event.getUser(), event.getItemId());
     }
+
+    public List<String> getIgnoreProperties() {
+        return ignoreProperties;
+    }
+    
 }
