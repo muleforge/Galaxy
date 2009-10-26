08f7aad339aa9b774849c515c14c069856e58199
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2009-10-26 20:48:26 +0000

    Some improvements to ShowableTabListener

--- gwt-client/src/main/java/org/mule/galaxy/web/client/util/ShowableTabListener.java
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/util/ShowableTabListener.java
@@ -6,6 +6,7 @@ package org.mule.galaxy.web.client.util;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.event.TabPanelEvent;
 import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import java.util.List;
@@ -16,19 +17,27 @@ import org.mule.galaxy.web.client.Showable;
 public class ShowableTabListener extends SelectionListener<TabPanelEvent> {
     private TabItem previous;
     private List<String> params;
+    private List<String> previousParams;
     private final ErrorPanel errorPanel;
+    private final List<String> tabNames;
+    private final TabPanel tabPanel;
     
-    public ShowableTabListener(ErrorPanel errorPanel, List<String> params) {
+    public ShowableTabListener(TabPanel tabPanel,
+                               ErrorPanel errorPanel, 
+                               List<String> params,
+                               List<String> tabNames) {
         super();
+        this.tabPanel = tabPanel;
         this.errorPanel = errorPanel;
         this.params = params;
+        this.tabNames = tabNames;
     }
 
     @Override
     public void componentSelected(TabPanelEvent ce) {
         TabItem item = ce.getItem();
         
-        if (item.equals(previous)) {
+        if (item.equals(previous) && params.equals(previousParams)) {
             return;
         }
         
@@ -47,5 +56,18 @@ public class ShowableTabListener extends SelectionListener<TabPanelEvent> {
         
         previous = item;
         item.layout();
+    }
+
+    public void setParams(List<String> params) {
+        this.previousParams = this.params;
+        this.params = params;
+    }
+
+    public void showTab(String tabName) {
+        int idx = tabNames.indexOf(tabName);
+        if (idx == -1) {
+            idx = 0;
+        }
+        tabPanel.setSelection(tabPanel.getItem(idx));
     }
 }
