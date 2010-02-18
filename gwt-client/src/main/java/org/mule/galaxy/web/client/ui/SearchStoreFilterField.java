01f833549fa317b8fff37a292a3b2f18439746ff
merv <merv@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2010-02-18 20:58:11 +0000

    make class abstract

--- gwt-client/src/main/java/org/mule/galaxy/web/client/ui/SearchStoreFilterField.java
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/ui/SearchStoreFilterField.java
@@ -1,10 +1,9 @@
 package org.mule.galaxy.web.client.ui;
 
 import com.extjs.gxt.ui.client.data.ModelData;
-import com.extjs.gxt.ui.client.store.Store;
 import com.extjs.gxt.ui.client.widget.form.StoreFilterField;
 
-public class SearchStoreFilterField<M extends ModelData> extends StoreFilterField {
+public abstract class SearchStoreFilterField<M extends ModelData> extends StoreFilterField {
 
     public SearchStoreFilterField() {
         super();
@@ -13,10 +12,5 @@ public class SearchStoreFilterField<M extends ModelData> extends StoreFilterFiel
         setWidth(300);
         setTriggerStyle("x-form-search-trigger");
         addStyleName("x-form-search-field");
-    }
-
-    @Override
-    protected boolean doSelect(Store store, ModelData modelData, ModelData modelData1, String s, String s1) {
-        return false;
     }
 }
