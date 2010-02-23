f43324335d30ccf3b65bacf9802b932c6f899278
merv <merv@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2010-02-23 22:44:55 +0000

    new helper panel

--- gwt-client/src/main/java/org/mule/galaxy/web/client/util/panel/PaddedContentPanel.java
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/util/panel/PaddedContentPanel.java
@@ -1,21 +1,18 @@
 package org.mule.galaxy.web.client.util.panel;
 
-import com.extjs.gxt.ui.client.Style;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 
-/**
- * A ContentPanel with some sensible defaults so it actually shows up.
- */
-public class BasicContentPanel extends ContentPanel {
+/* usually used as a child panel -- no heading */ 
+public class PaddedContentPanel extends ContentPanel {
 
-    public BasicContentPanel() {
+    public PaddedContentPanel() {
         super();
         setBodyBorder(false);
         setBorders(false);
-        setButtonAlign(Style.HorizontalAlignment.CENTER);
         setMonitorWindowResize(true);
         setAutoWidth(true);
         setAutoHeight(true);
+        setHeaderVisible(false);
+        addStyleName("padded-panel");
     }
-
 }
