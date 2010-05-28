0e12685cc961d54857996c117c6eaee05b6db52d
merv <merv@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2010-05-28 20:55:42 +0000

    core panel

---
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/ui/panel/PaddedFlowPanel.java
@@ -0,0 +1,10 @@
+package org.mule.galaxy.web.client.ui.panel;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+
+public class PaddedFlowPanel extends FlowPanel {
+    public PaddedFlowPanel() {
+        super();
+        setStyleName("padded-panel");
+    }
+}
