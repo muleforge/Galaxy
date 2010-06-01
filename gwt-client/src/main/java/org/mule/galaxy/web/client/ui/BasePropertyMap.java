d0c7a8e030bb4e566d642e4280d988309e3e3d6b
novotny <novotny@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2010-06-01 20:16:59 +0000

    refactor BasePropertyMap from Propertymap to be reusable

---
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/ui/BasePropertyMap.java
@@ -0,0 +1,53 @@
+package org.mule.galaxy.web.client.ui;
+
+import com.extjs.gxt.ui.client.data.BeanModelTag;
+
+import java.io.Serializable;
+
+public class BasePropertyMap implements Serializable, BeanModelTag {
+
+    private String name;
+    private String value;
+
+    // optional but useful for grouping grids  -- ie,  mule, system, etc
+    private String type;
+    
+    public BasePropertyMap() {
+    }
+
+    public BasePropertyMap(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public BasePropertyMap(String name, String value, String type) {
+        this.name = name;
+        this.value = value;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
