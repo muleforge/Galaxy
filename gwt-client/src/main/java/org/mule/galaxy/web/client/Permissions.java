53ec18c0b823463b55f4481a5b86cd534e6bebdf
dandiep <dandiep@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2010-11-23 00:28:42 +0000

    Add CE permissions for GWT

---
+++ gwt-client/src/main/java/org/mule/galaxy/web/client/Permissions.java
@@ -0,0 +1,10 @@
+package org.mule.galaxy.web.client;
+
+
+public class Permissions {
+
+    public static final String ITEM_READ = "READ_ITEM";
+    public static final String ITEM_MODIFY = "MODIFY_ITEM";
+    public static final String ITEM_DELETE = "DELETE_ITEM";
+
+}
