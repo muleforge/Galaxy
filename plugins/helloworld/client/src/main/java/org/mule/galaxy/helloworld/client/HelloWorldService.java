c755298c3d2b44195eda0167d278b95d0eebc666
karan.malhi <karan.malhi@a180d455-963b-0410-bd2a-f8bbb34fb4c7>
2011-10-04 22:44:17 +0000

    Updated pom's by setting the parent correctly. Fixes issues with maven 3. Removed plugins module, no longer needed

--- plugins/helloworld/client/src/main/java/org/mule/galaxy/helloworld/client/HelloWorldService.java
+++
@@ -1,7 +0,0 @@
-package org.mule.galaxy.helloworld.client;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-
-public interface HelloWorldService extends RemoteService  {
-    String sayHello(String name);
-}
--- plugins/helloworld/client/src/main/java/org/mule/galaxy/helloworld/client/HelloWorldServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package org.mule.galaxy.helloworld.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface HelloWorldServiceAsync {
-    void sayHello(String name, AsyncCallback<String> callback);
-}
